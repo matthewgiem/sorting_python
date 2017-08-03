4dc25cd55564f9c954ff22eec13af0f0fd770666
Matthew Giem <matthew.giem@gmail.com>
2017-08-02 23:29:51 -0700

    begin new algorythm

--- sort.py
+++ sort.py
@@ -41,3 +41,30 @@ def bubble_sort(arg):
     print(''.join(map(str,new_array)) == answer)
 
 bubble_sort(numbers_to_sort)
+
+def insersion_sort(arg):
+    unsorted_numbers = arg
+    array = []
+    new_array = []
+    array = sorted_numbers.split()
+    for x in array:
+        new_array.append(int(x))
+    # sort
+    answer = '2444678991011111112131616161717171819192023232426282929292929293133363838394040414142434546474747495151525357575859616262636363636464666667676868686870757777777879838788899191939495959699100100'
+    i = 0
+    x = 0
+    times = 0
+    for x in range(0, len(new_array) - 1):
+        for i in range(0, len(new_array) - 1):
+            if new_array[i] > new_array[i + 1]:
+                num1 = new_array[i]
+                num2 = new_array[i + 1]
+                new_array[i] = num2
+                new_array[i + 1] = num1
+            i += 1
+            times += 1
+            if i == len(new_array) - x:
+                break
+        x += 1
+
+    pass
