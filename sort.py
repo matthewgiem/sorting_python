bf6a22a7f8a3e6b8c8105e1f44f12e4c82edf38b
Matthew Giem <matthew.giem@gmail.com>
2017-08-04 13:57:57 -0700

    working on insertion sort

--- sort.py
+++ sort.py
@@ -8,6 +8,17 @@ numbers_to_sort = open_doc.read()
 # close the doc
 open_doc.close()
 
+answer = '2444678991011111112131616161717171819192023232426282929292929293133363838394040414142434546474747495151525357575859616262636363636464666667676868686870757777777879838788899191939495959699100100'
+print("the numbers to sort are")
+print(numbers_to_sort)
+numbers_unordered = []
+array = []
+array = numbers_to_sort.split()
+for x in array:
+    numbers_unordered.append(int(x))
+print(numbers_unordered)
+
+
 # sorting algorythm
 def bubble_sort(arg):
     sorted_numbers = arg
@@ -17,7 +28,6 @@ def bubble_sort(arg):
     for x in array:
         new_array.append(int(x))
     # sort
-    answer = '2444678991011111112131616161717171819192023232426282929292929293133363838394040414142434546474747495151525357575859616262636363636464666667676868686870757777777879838788899191939495959699100100'
     i = 0
     x = 0
     times = 0
@@ -41,6 +51,7 @@ def bubble_sort(arg):
     print(''.join(map(str,new_array)) == answer)
 
 # bubble_sort(numbers_to_sort)
+print("--------")
 
 def insersion_sort(arg):
     unsorted_numbers = arg
@@ -51,24 +62,51 @@ def insersion_sort(arg):
         new_array.append(int(x))
     # sort
     answer = '2444678991011111112131616161717171819192023232426282929292929293133363838394040414142434546474747495151525357575859616262636363636464666667676868686870757777777879838788899191939495959699100100'
-    i = 0
-    x = 0
-    j = 0
+    i = 1
+    x = 1
     times = 0
-    for x in range(0, len(new_array) - 1):
-        for i in range(0, len(new_array) - 1):
-            if new_array[i] > new_array[i + 1]:
-                num1 = new_array[i]
-                num2 = new_array[i + 1]
-                new_array[i] = num2
-                new_array[i + 1] = num1
-                if j in range(0, len(new_array) - x):
-                    
+    for x in range(1, len(new_array) - 1):
+        
 
-            i += 1
-            times += 1
-            if i == len(new_array) - x:
-                break
+
         x += 1
+
+
+
+
+
+
+
+
+
+
+
+
+        # for i in range(0, len(new_array) - 1):
+        #     if new_array[i] > new_array[i + 1]:
+        #         num1 = new_array[i]
+        #         num2 = new_array[i + 1]
+        #         new_array[i] = num2
+        #         new_array[i + 1] = num1
+        #         j = 0
+        #         if j in range(0, x - i):
+        #             num1 = new_array[i - j]
+        #             num2 = new_array[i - j - 1]
+        #             new_array[i - j] = num2
+        #             new_array[i - j - 1] = num1
+        #             if new_array[i - j] >= new_array[i - j - 1]:
+        #                 break
+        #             j += 1
+        #             times += 1
+        #     i += 1
+        #     if i == len(new_array) - x:
+        #         break
+        # x += 1
+
+    print("it took the insertion sort algorythm " + str(times) + " times to sort out of the max amout of times " + str(math.pow(len(new_array) - 1, 2)))
+    print(times)
+    print(new_array)
+    print(''.join(map(str,new_array)) == answer)
+
 
 insersion_sort(numbers_to_sort)
