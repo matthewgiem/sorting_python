6c41e7915b46808940e40114c3b1f411b8f37841
Matthew Giem <matthew.giem@gmail.com>
2017-08-05 09:43:03 -0700

    finished with instertion sort

--- sort.py
+++ sort.py
@@ -50,7 +50,7 @@ def bubble_sort(arg):
     print(new_array)
     print(''.join(map(str,new_array)) == answer)
 
-# bubble_sort(numbers_to_sort)
+bubble_sort(numbers_to_sort)
 print("--------")
 
 def insersion_sort(arg):
@@ -62,47 +62,21 @@ def insersion_sort(arg):
         new_array.append(int(x))
     # sort
     answer = '2444678991011111112131616161717171819192023232426282929292929293133363838394040414142434546474747495151525357575859616262636363636464666667676868686870757777777879838788899191939495959699100100'
-    i = 1
+    i = 0
     x = 1
     times = 0
-    for x in range(1, len(new_array) - 1):
-        
-
-
+    for x in range(1, len(new_array)):
+        for i in range(0, len(new_array) - (len(new_array) - x)):
+            if new_array[x - i] < new_array[x - 1 - i]:
+                num1 = new_array[x - i]
+                num2 = new_array[x - 1 - i]
+                new_array[x - i] = num2
+                new_array[x - 1 - i] = num1
+            i += 1
+            times += 1
+            if new_array[x - i] >= new_array[x - 1 - i]:
+                break
         x += 1
-
-
-
-
-
-
-
-
-
-
-
-
-        # for i in range(0, len(new_array) - 1):
-        #     if new_array[i] > new_array[i + 1]:
-        #         num1 = new_array[i]
-        #         num2 = new_array[i + 1]
-        #         new_array[i] = num2
-        #         new_array[i + 1] = num1
-        #         j = 0
-        #         if j in range(0, x - i):
-        #             num1 = new_array[i - j]
-        #             num2 = new_array[i - j - 1]
-        #             new_array[i - j] = num2
-        #             new_array[i - j - 1] = num1
-        #             if new_array[i - j] >= new_array[i - j - 1]:
-        #                 break
-        #             j += 1
-        #             times += 1
-        #     i += 1
-        #     if i == len(new_array) - x:
-        #         break
-        # x += 1
-
     print("it took the insertion sort algorythm " + str(times) + " times to sort out of the max amout of times " + str(math.pow(len(new_array) - 1, 2)))
     print(times)
     print(new_array)
