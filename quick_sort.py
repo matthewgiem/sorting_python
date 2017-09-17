1b2558bfcb625279ce08d5b47b38d25ecd5d5052
Matthew Giem <matthew.giem@gmail.com>
2017-09-16 17:31:33 -0700

    saturday work

--- quick_sort.py
+++ quick_sort.py
@@ -1,9 +1,22 @@
+import random
 def quick_sort(arg):
     array = list(arg[0])
     type_of_data = arg[1]
     times = 0
     swaps = 0
-    for i in range(len(array) - 1):
-        if array[i] > array[i + 1]:
-            array[i], array[i + 1] = array[i + 1], array[i]
+    # pick partition
+    partition = random.randint(0,len(array) - 1)
+    print("partition = {}".format(array[partition]))
+    less = []
+    greater = []
+    for i in range(len(array)):
+        print(array[i])
+        if i != partition:
+            if array[i] >= array[partition]:
+                greater.append(array[i])
+            else:
+                less.append(array[i])
+    print(less, array[partition], greater)
+
+
     return array, times, "Quick Sort", swaps, arg[0], type_of_data
