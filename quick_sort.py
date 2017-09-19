f86fa14c60289d37b4a52e119c8c850904904053
Matthew Giem <matthew.giem@gmail.com>
2017-09-18 20:27:29 -0700

    fucking bull shit .append() returns non FUCK MY LIFE

--- quick_sort.py
+++ quick_sort.py
@@ -6,17 +6,25 @@ def quick_sort(arg):
     swaps = 0
     # pick partition
     partition = random.randint(0,len(array) - 1)
-    print("partition = {}".format(array[partition]))
+    # print("partition = {}".format(array[partition]))
     less = []
     greater = []
     for i in range(len(array)):
-        print(array[i])
         if i != partition:
             if array[i] >= array[partition]:
                 greater.append(array[i])
             else:
                 less.append(array[i])
-    print(less, array[partition], greater)
+    less.append(array[partition])
+    new_array = less + greater
+    # print(type(less))
+    # print(type(array[partition]))
+    # print(less)
+    # print(array[partition])
+    # print(greater)
+    # print("{} + {} = {}".format(less, array[partition], less.append(array[partition])))
 
+    # answer = less.append(array[partition]) + greater
+    # print(answer)
 
-    return array, times, "Quick Sort", swaps, arg[0], type_of_data
+    return new_array, times, "Quick Sort", swaps, arg[0], type_of_data
