7d3b265de143f808293d2b4db576ee58df9254d8
Matthew Giem <matthew.giem@gmail.com>
2017-09-26 14:54:50 -0700

    median of 3 random elemets new partition working correctly

--- new_quick_sort.py
+++ new_quick_sort.py
@@ -1,19 +1,31 @@
 import random
+from numpy import median
 
 def new_quick_sort(arg):
     type_of_data = arg[1]
     array = list(arg[0])
     print(array)
-    # print(quick_sort2(array, 0, len(array) - 1))
-    print(quick_sort2(array, 4, 15))
+    print(quick_sort2(array, 0, len(array) - 1))
+    # print(quick_sort2(array, 4, 15))
 
 def quick_sort2(array, low, high):
-    # print("array = {}, low = {}, high = {}".format(array, low, high))
-    # print(range(low, high + 1))
     j = low
+    # print(array[low:high])
+    # list_of_3 = random.sample(array[low:high])
+    # print(list_of_3)
+    # median = int(median(list_of_3))
+    # print(median)
+    # partition = median
+    # partition = int(median(random.sample(array[low:high], 3)))
+    list_of_3 = random.sample(array[low:high], 3)
+    middle = median(list_of_3)
+    integer = int(middle)
+    partition = array.index(integer)
+    array[-1], array[partition] = array[partition], array[-1]
     for i in range(low, high + 1):
+        if i == high:
+            partition = j
         if array[i] <= array[high]:
             array[i], array[j] = array[j], array[i]
             j += 1
-
-    return array
+    return array, partition
--- sort.py
+++ sort.py
@@ -32,7 +32,7 @@ arg = [random_numbers, "large group of random numbers"]
 test = [[10, 9, 8, 7, 6, 5, 4, 3, 2, 1], "reversed"]
 almost = [[10, 2, 3, 1, 4, 3, 5, 10, 8, 9], "few swaps"]
 random_ten = [[2,5,1,7,9,3,10,4,6,8], "random 10"]
-random_twenty = [[3,5,2,6,8,1,9,6,2,9,2,0,1,3,4,7,2,9,10,4], "random 20"]
+random_twenty = [[3,5,2,6,8,1,9,6,2,9,2,0,1,3,8,7,2,9,10,4], "random 20"]
 # print(return_median_of_3(test[0]))
 # finished
 # ----------------------- #
