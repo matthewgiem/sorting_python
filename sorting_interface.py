8d8ddefe574d622c651102efe0e4513329d5cc74
Matthew Giem <matthew.giem@gmail.com>
2017-10-07 13:29:31 -0700

    saturday commit

--- sorting_interface.py
+++ sorting_interface.py
@@ -1,6 +1,28 @@
+def list_of_algorythms():
+    print('''
+    1.  Bubble Sort
+    2.  Insertion Sort
+    3.  Quick Sort
+    4.  Optimized version of Bubble Sort
+    5.  Optimized version of Insertion Sort
+    ''')
+
+def what_type_of_data():
+    while True:
+        number_of_integers = int(raw_input("how many data points would you like > "))
+        if number_of_integers < 10:
+            print("you need to choose a value greater than or equal to 10")
+        else:
+            return number_of_integers
+
 while True:
-    print("this is the instructions page")
+    print("Welcom to my sorting program")
+    print("The use of this is to demonstrat my profeciency using sorting algorythms")
+    print("If at any time you wish to exit the program just enter 'STOP' in to the prompt")
+    list_of_algorythms()
     choice = raw_input("what algorythm do you wanna use? ")
     if choice == "STOP":
         print("thanks for using the sorting service")
         break
+    what_type_of_data()
+    print("end of program")
